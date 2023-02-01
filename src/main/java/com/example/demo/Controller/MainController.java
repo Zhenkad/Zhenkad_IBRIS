aad04f2c9dc978a7a5e1e89c80027d92cfd2609a
Zhenkad <evgenii200127@gmail.com>
2023-02-01 12:01:54 +1000

    First commit

--- src/main/java/com/example/demo/Controller/MainController.java
+++ src/main/java/com/example/demo/Controller/MainController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String hello(){
-        return "Hello, Spring!";
+        return "Hello, Spring! Test Message :-)";
     }
 }
